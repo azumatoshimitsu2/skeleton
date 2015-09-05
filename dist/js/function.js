f648203f41bdeb73c2d38aea9e8e0ee050606652
Azuma <kangaroonote@gmail.com>
2015-09-05 23:22:06 +0900

    クラス名、gulpfile 更新

--- dist/js/function.js
+++ dist/js/function.js
@@ -1,41 +1,39 @@
-/**
- * @fileOverview サイト全体で使用する 関数
- * @externs
- *
- */
+$(document).ready(function () {
 
-$(document).ready(init);
+	/**
+	 * @画像名の末尾に -off がつくものをマウスオーバーで末尾 -on の画像に切り替え
+	 *
+	 * @return {undefined}
+	 */
+	var rollover = function () {
+		$('img').mouseover( function () {
+			var src = $(this).attr('src').replace('-off.', '-on.');
+			$(this).attr('src', src);
+		});
 
-function init() {
-	rollover();
-	smoothScroll();
-};
-	
-/**
- * @画像名の末尾に -off がつくものをマウスオーバーで末尾 -on の画像に切り替え
- *
- * @return {undefined}
- */
-var  rollover = function() {
-	$('img').bind('mouseover', function() {
-		var src = $(this).attr('src').replace('-off.', '-on.');
-		$(this).attr('src', src);
-	});
+		$('img').mouseout( function () {
+			var src = $(this).attr('src').replace('-on.', '-off.');
+			$(this).attr('src', src);
+		});
+	}
 
-	$('img').bind('mouseout', function() {
-		var src = $(this).attr('src').replace('-on.', '-off.');
-		$(this).attr('src', src);
-	});
-}
+	/**
+	 * @ページ内リンクをアニメーション
+	 *
+	 * @return {undefined}
+	 */
+	var smoothScroll = function () {
+		$('.totop a').click( function (e) {
+			e.preventDefault();
+			$('html, body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'fast');
+		});
+	}
 
-/**
- * @ページ内リンクをアニメーション
- *
- * @return {undefined}
- */
-var  smoothScroll = function() {
-	$(".totop a").click(function(e) {
-		e.preventDefault();
-		$('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'fast');
-	});
-}
+	function initialize () {
+		rollover();
+		smoothScroll();
+	}
+
+	initialize();
+
+});
