6781f22400d0fc564ea2aebe5d344dc0647cd7b5
azuma <access_post@yahoo.co.jp>
2017-03-13 11:49:51 +0900

    gulpにスタイルガイドを追加 EJS 削除

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,10 +1,11 @@
-var gulp    = require('gulp');
-var compass = require('gulp-compass');
-var rename = require('gulp-rename');
-var ejs = require("gulp-ejs");
-var fs = require("fs");
-var uglify = require("gulp-uglify");
-var concat = require("gulp-concat");
+var gulp       = require('gulp');
+var compass    = require('gulp-compass');
+var rename     = require('gulp-rename');
+var fs         = require("fs");
+var uglify     = require("gulp-uglify");
+var concat     = require("gulp-concat");
+var styleguide = require('sc5-styleguide');
+var outputPath = '../styleguide/';
  
 gulp.task('compass', function(){
     gulp.src('sass/*.scss').pipe(compass({
@@ -20,7 +21,7 @@ gulp.task('concat', function() {
         .pipe(concat('lib.js'))
         .pipe(gulp.dest('./js'));
 });
-gulp.task('minify', function() {
+gulp.task('minify', ['concat'], function() {
     return gulp.src('./js/lib.js')
         .pipe(uglify())
         .pipe(rename('lib.min.js')) 
@@ -35,29 +36,38 @@ gulp.task('watch', function(){
     });
 });
 
-gulp.task('ejs', function(){
-    var jsonData = require('./json/pages.json');
-    
-    jsonData.pages.forEach(function (data, index) {
-        //PC
-        gulp.src("./ejs/template.ejs")
-           .pipe(ejs({
-                data:{
-                    title: data.title,
-                    imageSrc: data.imageSrc,
-                }
-            }))
-            .pipe(rename(data.page+".html"))
-            .pipe(gulp.dest("../dist/"+data.dir));
-    });
+gulp.task('styleguide:generate', function() {
+  return gulp.src('sass/*.scss')
+    .pipe(styleguide.generate({
+        title: 'My Styleguide',
+        server: true,
+        rootPath: outputPath,
+        showCss: false,
+        overviewPath: outputPath + 'README.md',
+        extraHead: '<style>html, body { font-size: 10px; } .sg.sg-wrapper { max-width: 1000px; }</style>',
+        afterBody: '<script>document.getElementsByClassName("view-index")[0].setAttribute("id", "container")</script>'
+      }))
+    .pipe(gulp.dest(outputPath));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('sass/*.scss', function(event) {
-        gulp.run('compass');
-    });
+gulp.task('styleguide:applystyles', function() {
+  return gulp.src('sass/*.scss')
+    .pipe(compass({
+      config_file: 'config.rb',
+      comments: false,
+      sass: 'sass/',
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest(outputPath));
 });
-  
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+
+gulp.task('watch', ['styleguide'], function() {
+  gulp.watch(['sass/*.scss'], ['styleguide']);
+});
+
 gulp.task('default', function(){
     gulp.run('watch');
 });
