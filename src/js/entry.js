ae628499d5c406506d03db05086ea732108843d2
azuma <access_post@yahoo.co.jp>
2017-12-25 12:43:39 +0900

    デフォルトの JS 調整

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,36 +1,20 @@
-import $ from 'jquery/dist/jquery.min.js'
+import $ from 'jquery'
 
 $(document).ready(function () {
+	
+	$('a:not(".no-scroll")').click( function (e) {
 
-	var es6_test = (x, y, z) => { console.log(x, y, z); };
-	es6_test('a', 'b', 'c');
+		let href = $(this).attr('href');
+		if(href.match(/^#/) && $(href).length > 0) {
+			e.preventDefault()
+			$('html, body').animate({ scrollTop: $(href).offset().top }, 'fast')
+		}
+	})
 
-	var smoothScroll = function () {
-		$('a').click( function (e) {
-
-			var href = $(this).attr('href');
-			if(href.match(/^#/) && $(href).length > 0) {
-				e.preventDefault();
-				$('html, body').animate({ scrollTop: $(href).offset().top }, 'fast');
-			}
-		});
-	}
-
-	var toggleNav = function() {
-
-		$(".toggle-nav").click(function(e) {
-			e.preventDefault();
-			$(".toggle-nav").toggleClass("open");
-			$(".nav-main").slideToggle(250);
-
-		});
-	}
-
-	function initialize () {
-		smoothScroll();
-		toggleNav();
-	}
-
-	initialize();
+	$(".toggle-nav").click(function(e) {
+		e.preventDefault()
+		$(".toggle-nav").toggleClass("open")
+		$(".nav-main").slideToggle(250)
+	})
 
 });
