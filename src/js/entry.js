d5ee0c17d8043559998f503fc3dc58efcae30f17
azuma <access_post@yahoo.co.jp>
2018-04-16 15:32:55 +0900

    Webpack 追加

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,35 +1,11 @@
-import umbrella from 'umbrellajs'
-import jump from 'jump.js'
-import ua from './modules/ua.js'
-import sliderToggle from './modules/slideToggle.js'
+// JavaScriptモジュール
+import ua from './modules/ua.js';
+
+// SCSSソース　（SASSはJavaScriptで一度インポートする必要があります!）
+import '../sass/style.scss';
+
 
 (function() {
-	const u = umbrella.u
-	const ajax = umbrella.ajax
+	console.log('loading: entry.js')
 
-	if(u('#container').size().width > 640) {
-		u('meta[name="viewport"]').attr('content', 'width=1100')
-	}
-	
-	if(!ua.Mobile){
-	    u('a[href^="tel:"]').addClass('reset-a-style').on('click', function(e) {
-	        e.preventDefault()
-	    })
-	}
-
-	u('a[href^="#"]').handle('click', function(e) {
-		jump(u(this).attr('href'), {
-			duration: 400
-		})
-	})
-
-	u(".button--toggle").handle('click', function(e) {
-		u(this).toggleClass('button--toggle--open')
-		u('.nav-main').toggleClass('nav-main--show')
-	})
-
-	u('.compo-toggle').each(function(v, i) {
-		sliderToggle(v)
-	})
-})()
-
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,85 @@
-const webpack = require("webpack");
+// webpack v4
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-        js: __dirname + "/src/js/entry.js"
+        // JavaScriptソース
+        main: "./src/js/entry.js"
     },
     output: {
-        path : __dirname + '/dist/assets/js',
-        filename: 'bundle.js'
+        // ベースフォルダ
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'assets/js/bundle.js',
     },
     resolve: {
         extensions: ['.js']//importしているファイルの拡張子
     },
     module: {
-        loaders: [
+        rules: [
+            // JavaScriptルール(ESLint)を使う
+            // {
+            //     enforce: "pre",
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: "eslint-loader",
+            // },
+            // BabelがJavaScriptを翻弄する
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            // SASSからCSSを作成する
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            //file-loaderがsrcからdistフォルダにイメージをコピーする（webpack-serverに必要）
+            //{
+            //    test: /\.(jpg|png|gif|svg|ico)$/,
+            //    use: [
+            //        {
+            //            loader: 'file-loader',
+            //            options: {
+            //                name: '[name].[ext]',
+            //                outputPath: 'img/'
+            //            }
+            //        }
+            //    ]
+            //}
         ]
     },
     plugins: [
-    ]
+        // CSS
+        new ExtractTextPlugin("assets/css/styles.css"),
+        // HTML
+        //new HtmlWebpackPlugin({
+        //    inject: false, // CSSとJavaScriptを自動的に挿入する
+        //    hash: true,
+        //    template: './src/index.html',
+        //    filename: './index.html'
+        //}),
+        // srcからdistフォルダに画像をコピーする
+        //new CopyWebpackPlugin([
+        //    {
+        //        from: 'src/img',
+        //        to: 'assets/img'
+        //    } 
+        //]),
+        // srcフォルダからdistフォルダにJavaScriptのlibs（例：jQuery）をコピーする
+        new CopyWebpackPlugin([
+            {
+                from: 'src/js/libs',
+                to: 'assets/js'
+            } 
+        ]),
+    ],
 };
