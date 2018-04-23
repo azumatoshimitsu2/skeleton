3619a492fe6c7c34f5df606075e998feab93f5a6
MattHofmann <mhofm@me.com>
2018-04-23 11:49:37 +0900

    Add Stylelint (CSS)

--- dist/assets/js/bundle.js
+++ dist/assets/js/bundle.js
@@ -1,1 +1,109 @@
-!function(e){var n={};function i(o){if(n[o])return n[o].exports;var t=n[o]={i:o,l:!1,exports:{}};return e[o].call(t.exports,t,t.exports,i),t.l=!0,t.exports}i.m=e,i.c=n,i.d=function(e,n,o){i.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:o})},i.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},i.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return i.d(n,"a",n),n},i.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},i.p="",i(i.s=1)}([function(e,n,i){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=function(){var e=window.navigator.userAgent.toLowerCase();return{Tablet:-1!=e.indexOf("windows")&&-1!=e.indexOf("touch")||-1!=e.indexOf("ipad")||-1!=e.indexOf("android")&&-1==e.indexOf("mobile")||-1!=e.indexOf("firefox")&&-1!=e.indexOf("tablet")||-1!=e.indexOf("kindle")||-1!=e.indexOf("silk")||-1!=e.indexOf("playbook"),Mobile:-1!=e.indexOf("windows")&&-1!=e.indexOf("phone")||-1!=e.indexOf("iphone")||-1!=e.indexOf("ipod")||-1!=e.indexOf("android")&&-1!=e.indexOf("mobile")||-1!=e.indexOf("firefox")&&-1!=e.indexOf("mobile")||-1!=e.indexOf("blackberry")}}},function(e,n,i){"use strict";var o,t=i(0);(o=t)&&o.__esModule;i(6),console.log("loading: entry.js")},,,,,function(e,n){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/entry.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/entry.js":
+/*!*************************!*\
+  !*** ./src/js/entry.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nvar _ua = __webpack_require__(/*! ./modules/ua.js */ \"./src/js/modules/ua.js\");\n\nvar _ua2 = _interopRequireDefault(_ua);\n\n__webpack_require__(/*! ../sass/styles.scss */ \"./src/sass/styles.scss\");\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n// JavaScriptモジュール\n(function () {\n\tconsole.log('loading: entry.js');\n})();\n\n// SCSSソース　（SASSはJavaScriptで一度インポートする必要があります!）\n\n//# sourceURL=webpack:///./src/js/entry.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/ua.js":
+/*!******************************!*\
+  !*** ./src/js/modules/ua.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nexports.default = function () {\n  var u = window.navigator.userAgent.toLowerCase();\n  return {\n    Tablet: u.indexOf(\"windows\") != -1 && u.indexOf(\"touch\") != -1 || u.indexOf(\"ipad\") != -1 || u.indexOf(\"android\") != -1 && u.indexOf(\"mobile\") == -1 || u.indexOf(\"firefox\") != -1 && u.indexOf(\"tablet\") != -1 || u.indexOf(\"kindle\") != -1 || u.indexOf(\"silk\") != -1 || u.indexOf(\"playbook\") != -1,\n    Mobile: u.indexOf(\"windows\") != -1 && u.indexOf(\"phone\") != -1 || u.indexOf(\"iphone\") != -1 || u.indexOf(\"ipod\") != -1 || u.indexOf(\"android\") != -1 && u.indexOf(\"mobile\") != -1 || u.indexOf(\"firefox\") != -1 && u.indexOf(\"mobile\") != -1 || u.indexOf(\"blackberry\") != -1\n  };\n};\n\n//# sourceURL=webpack:///./src/js/modules/ua.js?");
+
+/***/ }),
+
+/***/ "./src/sass/styles.scss":
+/*!******************************!*\
+  !*** ./src/sass/styles.scss ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./src/sass/styles.scss?");
+
+/***/ })
+
+/******/ });
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -58,6 +59,8 @@ module.exports = {
         ]
     },
     plugins: [
+        // CSS Stylelint
+        new StyleLintPlugin({/* Options */}),
         // CSS
         new ExtractTextPlugin({
             filename: "assets/css/styles.css"
