3d71dce83a0d8cf8159a6ecaebd1920d7eead9ba
azuma <access_post@yahoo.co.jp>
2018-04-25 15:11:37 +0900

    VViewport の動的書き換え 電話番号リンクの処理追加

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,11 +1,18 @@
 // JavaScriptモジュール
 import ua from './modules/ua.js';
-
 // SCSSソース　（SASSはJavaScriptで一度インポートする必要があります!）
 import '../sass/styles.scss';
 
+$(document).ready(function () {
+	let ua = u()
 
-(function() {
-	console.log('loading: entry.js')
+	if(!ua.Mobile){
+	    $('a[href^="tel:"]').addClass("reset-a-style").on('click', function(e) {
+	        e.preventDefault();
+	    });
+	}
 
-})();
+	if($(window).width() >= 720) {
+		$('meta[name="viewport"]').attr('content', 'width=1360');
+	}
+})
