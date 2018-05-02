aa016752d39d64fac449aa2a8ffe39eb9089a4b6
azuma <access_post@yahoo.co.jp>
2018-05-02 18:52:59 +0900

    微調整

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,13 +1,13 @@
 // JavaScriptモジュール
-import ua from './modules/ua.js';
+import uajs from './modules/ua.js';
 // SCSSソース　（SASSはJavaScriptで一度インポートする必要があります!）
 import '../sass/styles.scss';
 
 $(document).ready(function () {
-	let ua = u()
+	let ua = uajs()
 
 	if(!ua.Mobile){
-	    $('a[href^="tel:"]').addClass("reset-a-style").on('click', function(e) {
+	    $('a[href^="tel:"]').addClass('reset-a-style').on('click', function(e) {
 	        e.preventDefault();
 	    });
 	}
