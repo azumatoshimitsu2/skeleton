70b41be67c06936dd6f051b876dd00ec71bae194
Azuma <kangaroonote@gmail.com>
2019-01-05 16:29:42 +0900

    package.json 追加 rollup.js で JavaScript をバンドル

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+
+export default {
+  input: 'src/js/main.js',
+  output: {
+    file: 'dist/assets/js/bundle.js',
+    format: 'cjs',
+    compact: true
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }), // ES5に変換
+    uglify()
+  ]
+};
