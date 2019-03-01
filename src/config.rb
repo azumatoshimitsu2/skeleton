fbe115d6eeb28b08c24422e3bd8ec9b864d43ebd
azuma <access_post@yahoo.co.jp>
2019-03-01 17:57:29 +0900

    config.rb 削除

--- src/config.rb
+++
@@ -1,8 +0,0 @@
-http_path = "/"
-css_dir = "../dist/assets/css"
-sass_dir = "sass"
-
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
-
-line_comments = false
