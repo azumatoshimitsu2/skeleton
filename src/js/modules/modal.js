6ceef3935c6e1910f3065b646a4c7d793af6560b
azuma <access_post@yahoo.co.jp>
2019-03-18 19:37:47 +0900

    JavaScript モーダルモジュール追加

---
+++ src/js/modules/modal.js
@@ -0,0 +1,28 @@
+export default function(){
+
+  // モーダル HTML Sample
+  // <div class="modal-trigger" data-target="modal-window-0">押すとモーダルウィンドウがでるよ</div>
+  // <div id="modal-window-0" class="modal-window">
+  //   <div class="modal-close-trigger">押すとモーダルウィンドウが消えるよ</div>
+  //   <div class="modal-window__content">
+  //     モーダルウィンドウの内容
+  //   </div>
+  // </div>
+
+  $('body:eq(0)').append('<div id="modal-overlay" class="c-modal-overlay">');
+
+  $('.modal-trigger').on('click', function(e) {
+    e.preventDefault();
+    let $target = $($(this).attr('data-target'));
+    let scTop = $(window).scrollTop();
+    $target.css({'top' : (scTop + 100) + 'px' });
+    $target.fadeIn(250);
+    $('#modal-overlay').fadeIn(250);
+  });
+
+  $('.modal-close-trigger, #modal-overlay').on('click', function(e) {
+    $('.modal-window').fadeOut(250);
+    $('#modal-overlay').fadeOut(250);
+  });
+
+}
