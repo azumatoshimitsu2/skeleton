300b8565957f59cd81a4e96954d268dc52fc0f8f
Azuma Toshimitsu <azuma@caracriworks.com>
2019-11-06 16:42:16 +0900

    sass import の glob 対応

--- index.js
+++ index.js
@@ -2,61 +2,56 @@ const cmd = process.argv[2]
 const sass = require('node-sass');
 const fs = require('fs-extra')
 const path = require('path');
-const chokidar = require('chokidar')
+const chokidar = require('chokidar');
+var globImporter = require('node-sass-glob-importer');
 
-switch (cmd) {
-  case 'recursiveSassCompile':
-    const src = __dirname + '/src/sass/';
-
-    const watcher = chokidar.watch(src, {
-      persistent: true
+const src = __dirname + '/src/sass/';
+const watcher = chokidar.watch(src, {
+  persistent: true
+});
+const walker = function (p, fileCallback, errCallback) {
+  fs.readdir(p, function (err, files) {
+    if (err) {
+      errCallback(err);
+      return;
+    }
+    files.forEach(function (f) {
+      let fp = path.join(p, f);
+      if (fs.statSync(fp).isDirectory()) {
+        walker(fp, fileCallback);
+      } else {
+        fileCallback(fp);
+      }
     });
-
-    const walker = function (p, fileCallback, errCallback) {
-      fs.readdir(p, function (err, files) {
-        if (err) {
-          errCallback(err);
-          return;
-        }
-        files.forEach(function (f) {
-          let fp = path.join(p, f);
-          if (fs.statSync(fp).isDirectory()) {
-            walker(fp, fileCallback);
-          } else {
-            fileCallback(fp);
-          }
-        });
-      });
-    };
-
-    const compleScss = function (scss_filename) {
-      let name = path.basename(scss_filename);
-      if (name.endsWith('.scss') && !name.startsWith('_')) {
-        sass.render({
-          file: scss_filename,
-          includePaths: ['src/sass/'],
-          outputStyle: "compressed", //nested, expanded, compact
-        }, (error, result) => {
+  });
+};
+const compleScss = function (scss_filename) {
+  let name = path.basename(scss_filename);
+  if (name.endsWith('.scss') && !name.startsWith('_')) {
+    sass.render({
+      file: scss_filename,
+      includePaths: ['src/sass/'],
+      outputStyle: "compressed", //nested, expanded, compact
+      importer: globImporter()
+    }, (error, result) => {
+      if (error) {
+        console.log(error.status);
+        console.log(error.column);
+        console.log(error.message);
+        console.log(error.line);
+      }
+      else {
+        let dist = scss_filename.replace(".scss", ".css");
+        dist = dist.replace("/src/sass/", "/dist/assets/css/");
+        console.log('outputFile : ', dist);
+        fs.outputFile(dist, result.css, (error) => {
           if (error) {
-            console.log(error.status);
-            console.log(error.column);
-            console.log(error.message);
-            console.log(error.line);
-          }
-          else {
-            let dist = scss_filename.replace(".scss", ".css");
-            dist = dist.replace("/src/sass/", "/dist/assets/css/");
-            console.log('outputFile : ', dist);
-            fs.outputFile(dist, result.css, (error) => {
-              if (error) {
-                console.log(error);
-              }
-            });
+            console.log(error);
           }
         });
       }
-    }
-
-    watcher.on('ready', function () { console.log("chokidar ready"); })
-      .on('change', function (file) { walker(src, compleScss); console.log("Update : " + file); });
+    });
+  }
 }
+watcher.on('ready', function () { console.log("chokidar ready"); })
+  .on('change', function (file) { walker(src, compleScss); console.log("Update : " + file); });
