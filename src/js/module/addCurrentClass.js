6155cc52089d725452237688281384e452ebf73a
Azuma Toshimitsu <azuma@caracriworks.com>
2020-10-16 10:19:04 +0900

    JavaScript で使うクラス名変更

--- src/js/module/addCurrentClass.js
+++ src/js/module/addCurrentClass.js
@@ -3,12 +3,11 @@ export default function () {
   //現在のパスの .nav-main 内のリンクに .current を追加　.throwgh-current があるものは除く
   const path = location.pathname
   const mainLink = document.querySelectorAll('.nav a');
-  console.log(mainLink);
 
   mainLink.forEach( (v, i) => {
     if (v.getAttribute('href') == path) {
-      if (!v.classList.contains('throwgh-current')) {
-        v.classList.add('current');
+      if (!v.classList.contains('throwgh')) {
+        v.classList.add('is-current');
       }
     }
   });
