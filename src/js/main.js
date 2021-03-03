ce4e055cfca7bde93afe120a0af32bcc258fb8af
Azuma Toshimitsu <azuma@caracriworks.com>
2021-03-03 16:58:29 +0900

    .is-hover を追加する処理に変更

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ import accessibleGnavToggle from './module/accessibleGnavToggle';
 import addCurrentClass from './module/addCurrentClass';
 import smoothscroll from './module/jquery.smoothscroll';
 import pcViewportFixed from './module/pcViewportFixed';
-import spRemoveIshover from './module/spRemoveIshover';
+import spAddIshover from './module/spAddIshover';
 
 (function ($) {
   $(document).ready(function () {
     accessibleGnavToggle();
     addCurrentClass();
-    spRemoveIshover();
+    spAddIshover();
     pcViewportFixed();
     smoothscroll();
   });
---
+++ src/js/module/spAddIshover.js
@@ -0,0 +1,18 @@
+import uajs from './ua';
+
+export default function () {
+
+  // スマホタブレットでなければロールオーバーで使用する（二回タップ対策用）クラスを追加
+
+  const ua = uajs();
+
+  if (ua.Mobile || ua.Tablet) {
+  } else {
+    const hoverItems = document.querySelectorAll('a');
+    hoverItems.forEach( (v, i) => {
+      v.classList.add('is-hover');
+      console.log(v);
+    });
+  }
+
+}
