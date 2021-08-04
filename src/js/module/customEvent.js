c0bbc331ee1de6503677707e95544856a61cf87c
東　敏満 <azuma@caracriworks.com>
2021-08-04 17:01:20 +0900

    customEvent モジュール更新

--- src/js/module/customEvent.js
+++ src/js/module/customEvent.js
@@ -2,8 +2,15 @@ export default function (element, event, obj) {
 	if(!obj) {
 		obj = {};
 	}
-	element.dispatchEvent(new CustomEvent(event, {
-		detail: obj
-	  })
-	);
+
+	if ( typeof window.CustomEvent === "function" ) {
+		element.dispatchEvent(new CustomEvent(event, {
+			detail: obj
+			})
+		);
+	} else {
+		params = { bubbles: false, cancelable: false, detail: obj };
+		var evt = document.createEvent( 'CustomEvent' );
+		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+	}
 }
