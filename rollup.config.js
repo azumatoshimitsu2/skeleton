d767ccbaa2b44a0465867dc424871babe16dc1e2
東　敏満 <azuma@caracriworks.com>
2021-12-23 12:35:38 +0900

    非推奨 のrollup-plugin-babel-minify を rollup-plugin-terser に差し替え

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel';
-import minify from 'rollup-plugin-babel-minify';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/js/main.js',
@@ -15,6 +15,6 @@ export default {
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     }), // ES5に変換
-    // minify()
+    terser()
   ]
 };
